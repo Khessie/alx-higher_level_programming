799f9c08df5b2b14a36526e10ad6d87f9b8d20d6
Khessie <khessieevans@yahoo.com>
2022-10-11 19:57:30 -0700

    tasks added

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+able File  17 lines (15 sloc)  500 Bytes
+#!/usr/bin/python3
+"""
+Contains the class definition of a City
+"""
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class City(Base):
+    """
+    Class that defines each city
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
